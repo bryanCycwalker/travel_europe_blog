5bc01f2d3db05e73575e196ae350e892ab7b5721
bryanCycwalker <br.yuan@tum.de>
2026-05-11 09:45:46 +0200

    Create cityData.js

---
+++ cityData.js
@@ -0,0 +1,14 @@
+// cityData.js
+const cityData = [
+    {
+        id: 'siena', 
+        country: '🇮🇹 義大利 Italy', 
+        city: 'Siena 錫耶納', 
+        subtitle: '迷失在中世紀紅磚之城',
+        lat: 43.3188, 
+        lng: 11.3308, 
+        img: 'images/italy/siena/siena_main.jpg',
+        desc: '錫耶納獨旅攻略：比起佛羅倫斯的擁擠，這裡更適合一個人靜靜享受托斯卡尼陽光。整理必訪田野廣場與大教堂心得。', 
+        link: 'siena.html'
+    }
+];
