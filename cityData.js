b92d0a6a2f7b9ae638b2607484c6ad960abb54f9
bryanCycwalker <br.yuan@tum.de>
2026-05-12 12:34:34 +0200

    Update cityData.js

--- cityData.js
+++ cityData.js
@@ -1,6 +1,17 @@
 // cityData.js
 const cityData = [
     {
+        id: 'monteriggioni', 
+        country: '🇮🇹 義大利 Italy', 
+        city: 'Monteriggioni 蒙特里久尼', 
+        subtitle: '彷彿時光靜止的中世紀圓堡古城',
+        lat: 43.3900, 
+        lng: 11.2230, 
+        img: 'images/italy/monteriggioni/monteriggioni_main.jpg',
+        desc: '被但丁寫入《神曲》的防禦古城。獨旅蒙特里久尼最適合沿著圓形城牆漫步，在托斯卡尼的夕陽下，俯瞰金黃色的基安蒂丘陵與橄欖園。', 
+        link: 'cities/monteriggioni.html'
+    },
+    {
         id: 'siena', 
         country: '🇮🇹 義大利 Italy', 
         city: 'Siena 錫耶納', 
